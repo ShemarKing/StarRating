ac0e1c6f8ee412aa81ba0f80d4fd9bd0ceccc80d
zhangyaxun <zxy_0619@163.com>
2018-06-25 13:55:13 +0800

    Initial Commit

---
+++ ScorePrictice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScorePrictice
+//
+//  Created by apple on 2018/6/25.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
